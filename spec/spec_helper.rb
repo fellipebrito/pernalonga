1d6b60f9351410953dcdb9d0cc9bb72d6a17b295
Fellipe Brito <fellipe@devlandia.net>
2016-01-21 20:29:25 +0000

    Test Coverage + Fix syntax issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
+require 'dotenv'
+Dotenv.load
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+require 'simplecov'
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pernalonga'
